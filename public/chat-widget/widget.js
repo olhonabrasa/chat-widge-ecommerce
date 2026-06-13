698c1f8bd2cf8a20ea296a62905b7783b914dab2
Felipe Moreira <contato@revcoon.com.br>
2026-06-12 21:58:23 -0300

    Update backend URL for chat widget

--- public/chat-widget/widget.js
+++ public/chat-widget/widget.js
@@ -6,10 +6,10 @@
         textColor: '#ffffff',
         botName: 'Projetos Olho na Brasa',
         botAvatar: 'https://s3.1app.com.br/master/project_24727/xy6IrcJy1jkUGTlM4qSc7cF1suHmQyDE.jpg',
-        // backendUrl: 'https://chat-widget-tst.netlify.app/.netlify/functions/collect-lead', 
+        // backendUrl: 'https://chat-widget-olb.netlify.app/.netlify/functions/collect-lead', 
         backendUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
             ? 'http://localhost:8888/.netlify/functions/collect-lead'
-            : 'https://chat-widget-tst.netlify.app/.netlify/functions/collect-lead',
+            : 'https://chat-widget-olb.netlify.app/.netlify/functions/collect-lead',
         delays: {
             typing: 1000,
             message: 1500,
